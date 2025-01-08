2a693d7ee20cb05da268fe339fc3be5f996b9ec8
TonyBiele612 <tbiele@smm.org>
2025-01-08 15:16:23 -0600

    added retry on error

--- TransitSocketClient.py
+++ TransitSocketClient.py
@@ -1,13 +1,24 @@
 import socket
 import json
 import sys
+import time
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def CreateSocket():
     HOST = "127.0.0.1"  # Unity Server
     PORT = 5000
-    client_socket.connect((HOST, PORT))
+    retry_delay = 2
+    #client_socket.connect((HOST, PORT))
+
+    while True:
+        try:
+            client_socket.connect((HOST, PORT))
+            print("Connected")
+            return
+        except socket.error as e:
+            print(f"Connection failed: {e}. Retrying in {retry_delay} seconds...")
+            time.sleep(retry_delay)
 
 def SendData(data_list):
 
